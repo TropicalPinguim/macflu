66debd571f079e532197985ed8257675afbf17cf
TropicalPinguim <alan_3dworks@outlook.com>
2021-04-29 20:49:56 -0300

    Alan B

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from PyQt5 import uic, QtWidgets
 
-app = QtWidgets.QApplication([])
-screen = uic.loadUi("main_ux.ui")
-
-
 class calculo_pagina_1:
     def __init__(self, vazao, comprimento, perda, pressao):
         self.vazao = vazao
@@ -26,38 +22,30 @@ class calculo_pagina_1:
     def calcular(self):                               
         l1 = 10 * (((1.663785*((10)**(-3))*((self.vazao)**1.85)*self.comprimento)/(self.perda*self.pressao))**(1/5))
         l2 = (1.663785*((10)**(-3))*((self.vazao)**1.85)*self.comprimento)
-        l3 = (self.perda*self.pressao)  
+        l3 = (self.perda*self.pressao)
+        screen.label.setText(f'O Diametro interno é: {round(l1,2)}mm')  
         return [l1, l2, l3]
 
+    def ofConv(self): 
+        linha_01 = f"d=10[nroot!5?! !1.663785*10^-3* {self.vazao} ^1.85* {self.comprimento}? over ! {self.perda} * {self.pressao} ? ? ] NEWLINE "
+        linha_02 = f"d=10[nroot!5?! !{round(self.calcular()[1], 4)}? over !{round(self.calcular()[2], 4)}?? ] NEWLINE "
+        linha_03 = f"d= {round(self.calcular()[0], 2)} mm NEWLINE "
+        arquivo_office = linha_01.replace("!","{").replace("?","}") + linha_02.replace("!","{").replace("?","}") + linha_03
+        return [arquivo_office]
+
 def deposito_variavis():
-    Q = screen.vazao.text()#Vazão
-    c_t = screen.comprimento.text()#Comprimento total da tubulação
-    d_p = screen.perda.text()#Perda de pressão admissivel
-    p = screen.pressao.text()  #Pressão de trabalho  
+    Q = screen.vazao.text()             #Vazão
+    c_t = screen.comprimento.text()     #Comprimento total da tubulação
+    d_p = screen.perda.text()           #Perda de pressão admissivel
+    p = screen.pressao.text()           #Pressão de trabalho  
     okey = calculo_pagina_1(Q,c_t,d_p,p)
-    okey.calcular()
-    screen.label.setText(f'O Diametro interno é: {round(okey.calcular()[0],2)}mm')
-    return [Q, c_t, d_p, p, okey.calcular()[0],okey.calcular()[1], okey.calcular()[2]]
-
-def arquivar():
-    Q = deposito_variavis()[0]
-    c_t = deposito_variavis()[1]
-    d_p = deposito_variavis()[2]
-    p = deposito_variavis()[3]
-    d = str(round(deposito_variavis()[4],4))
-    l1 = str(round(deposito_variavis()[5],4))
-    l2 = str(round(deposito_variavis()[6],4))
-
-    linha_01 = f"d=10[nroot!5?! !1.663785*10^-3* {Q} ^1.85* {c_t}? over ! {d_p} * {p} ? ? ] NEWLINE "
-    linha_02 = f"d=10[nroot!5?! !{l1,4}? over !{l2}?? ] NEWLINE "
-    linha_03 = f"d= {d} mm NEWLINE "
-    arquivo_office = linha_01.replace("!","{").replace("?","}") + linha_02.replace("!","{").replace("?","}") + linha_03
-    return [arquivo_office]
+    okey.calcular()  
+    return [okey.ofConv()[0]]
 
 
 def salvar():
     arquivo = QtWidgets.QFileDialog.getSaveFileName()[0]
-    guardando = arquivar()[0]
+    guardando = deposito_variavis()[0]
     with open (arquivo + '.txt', 'w') as a:
         a.write(guardando)
 
@@ -79,16 +67,19 @@ def option_select():
         screen.label.setText(f'O Diametro interno é:')
 
 
-
 def screen_offs():
     screen.tela_1.close()
     screen.tela_2.close()
 
+app = QtWidgets.QApplication([])
+screen = uic.loadUi("main_ux.ui")
 
 screen_offs()
 screen.comboBox.addItems(["Escolha uma tela","Diametro interno da tubulação", "tela 2"])
+
 screen.bt_01.clicked.connect(option_select)
 screen.bt_02.clicked.connect(deposito_variavis)
+
 screen.actionSalvar.triggered.connect(salvar)
 
 screen.show()
